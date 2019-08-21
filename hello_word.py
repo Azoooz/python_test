755dc310dff968e47fa76824b69d1206de779f4a
iabdulaziz <abdulaziz.saleh.194@gmail.com>
2019-08-21 19:52:15 +0300

    secondDay

--- hello_word.py
+++ hello_word.py
@@ -1,5 +1,12 @@
+# First Day
+"""
 if ( 4 > 5 ):
     print(" the 4 greater than 5  ")
 
 else:
     print(" the 5 greater than 4 ")
+"""
+# second Day
+# print("hello word!")
+print("hello everyOne")
+
