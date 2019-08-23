0085e0157f6355d675b5c8c92e86ea450f25b1d5
iabdulaziz <abdulaziz.saleh.194@gmail.com>
2019-08-23 21:24:06 +0300

    four

--- hello_word.py
+++ hello_word.py
@@ -5,8 +5,42 @@ if ( 4 > 5 ):
 
 else:
     print(" the 5 greater than 4 ")
+#
+ second Day
+ print("hello word!")
+ print("hello everyOne")
+
+#third day
+
+# x = 100
+# x = 'ahmad'
+
+# x = 'nasser'
+# x = "nasser"
+#
+# print(x)
+
+x , y , z = 'nasser', 'yazzed' , 'ahmad'
+print(x, " - ", y, " - ", z)
+
+x = y = z = 'hammad'
+print(x, " - ", y, " - ", z)
+
+python = ' like it. '
+print("i" + python)
+
+x = "I"
+y = " Like it."
+z = x + y
+print(z)
+
+x = 10
+y = 13
+print(y + x)
+
+x = 22 ;
+y = "ahmad"
+print(y + x)
+
 """
-# second Day
-# print("hello word!")
-print("hello everyOne")
 
