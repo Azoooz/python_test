01b3222bb2e1c930b2f1bbf5061f4e5480dad279
Azoooz <abdulaziz.saleh.194@gmail.com>
2019-09-01 14:31:31 +0300

    day_9

---
+++ Day_7.py
@@ -0,0 +1,10 @@
+a = '''You can assign a multiline 
+string to a variable by using three quotes'''
+
+print(a)
+
+word = 'hello word'
+word1 = "hello word"
+
+print(word[2])
+print(word1[2])
---
+++ Day_8.py
@@ -0,0 +1,4 @@
+a = ' hello word '
+
+print(a.strip()) # return "hello word"
+print(len(a)) # return number of valu
